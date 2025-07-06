fd00bf7ef1415188da013985d0e13a5f10b7f49b
islam <islamamara062@gmail.com>
2025-07-07 00:54:35 +0100

    complet first part

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,26 @@
 import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
 
 const Weather = () => {
   return(
-    <div className="flex justify-center bg-blue-600 items-center">
+    <section className="flex justify-center items-center w-full h-screen">
+      <div className="bg-blue-600 h-100 p-10 w-100 rounded-lg">
 
-      <div className="flex gap-3.5">
-        <input type="text" placeholder="Search" className="bg-white rounded-lg"/>
-        <img src={search_icon} alt=""/>
+        <div className="flex justify-center gap-3">
+          <input type="text" placeholder="Enter a city." className="bg-white rounded-lg p-2" />
+          <img src={search_icon} alt="" className="bg-white rounded-full p-2.5"/> 
+        </div>
+        
+        <div className="flex-col flex text-center text-teal-50 text-lg justify-center items-center pt-10">
+          <img src={clear_icon} alt="" className="w-28 h-28"/>
+          <p>London</p>
+          <p>18C</p>
+          
+        </div>
       </div>
 
-    </div>
+      
+    </section>
   )
 }
 
